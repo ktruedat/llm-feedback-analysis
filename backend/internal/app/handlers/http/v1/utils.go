9d2af8e1b1da14986021272adc6ce221e91e6305
ktruedat <crinnic77@gmail.com>
2026-01-30 10:06:01 +0200

    feat: add parsing utility for handlers

--- backend/internal/app/handlers/http/v1/utils.go
+++ backend/internal/app/handlers/http/v1/utils.go
@@ -1,12 +1,44 @@
 package v1
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
 
+	"github.com/ktruedat/llm-feedback-analysis/internal/app/handlers/http/middleware"
+	"github.com/ktruedat/llm-feedback-analysis/internal/app/infrastructure/jwt"
+	"github.com/ktruedat/llm-feedback-analysis/internal/app/transport/requests"
 	ce "github.com/ktruedat/llm-feedback-analysis/pkg/errors"
 )
+
+type requestConstraint interface {
+	requests.CreateFeedbackRequest
+}
+
+type request[T requestConstraint] struct {
+	Claims *jwt.Claims
+	Data   T
+}
+
+func parsePayloadData[T requestConstraint](
+	r *http.Request,
+	body io.ReadCloser,
+) (*request[T], error) {
+	var req request[T]
+	if err := json.NewDecoder(body).Decode(&req.Data); err != nil {
+		return nil, fmt.Errorf("error decoding request body: %w", err)
+	}
+
+	if err := body.Close(); err != nil {
+		return &req, fmt.Errorf("error closing body: %w", err)
+	}
+
+	req.Claims = middleware.GetUserClaims(r)
+
+	return &req, nil
+}
 
 // handleSvcError handles service errors and converts them to appropriate HTTP responses.
 func (h *Handlers) handleSvcError(resp http.ResponseWriter, err error) {
